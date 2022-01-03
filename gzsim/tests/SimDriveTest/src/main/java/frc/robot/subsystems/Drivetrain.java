fc390371b64239fa4650666bc449d66ff5124051
alpine-robotics <dwsindorf@gmail.com>
2022-01-03 10:05:45 -0700

    fixed broken network bug: uncommented simulation.run at line 134 in Drivetrain.java

--- gzsim/tests/SimDriveTest/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ gzsim/tests/SimDriveTest/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -111,7 +111,7 @@ public class Drivetrain extends SubsystemBase implements Constants {
 		return Math.max(min, Math.min(value, max));
 	}
 	public double getTime(){
-		return simulation.getSimTime();
+		return simulation.getClockTime();
 	}
 	public void startAuto(){
 		simulation.reset();
@@ -131,7 +131,7 @@ public class Drivetrain extends SubsystemBase implements Constants {
 		simulation.end();
 	}
 	public void enable(){
-		//simulation.run();
+		simulation.run();
 		System.out.println("Drivetrain.enable");
 		leftMotor.enable();
 		rightMotor.enable();
